9f9469186349066c9947491610445e774977031e
Rimantas <belauzas@yahoo.com>
2021-01-22 20:50:39 +0200

    Render socials function validations #17

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 import { renderSocials } from './components/renderSocials.js';
 import { socialsData } from './data/socialsData.js';
 
-const socialsHTML = renderSocials(socialsData);
-
-console.log(socialsHTML);
+renderSocials('footer > .socials', socialsData);
