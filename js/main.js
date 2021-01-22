c7ce1f46506a5fe8a98cfd7793b4d4c10b80cdcc
Rimantas <belauzas@yahoo.com>
2021-01-22 19:39:27 +0200

    Render socials #17

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log('Coming soon');
+import { renderSocials } from './components/renderSocials.js';
+import { socialsData } from './data/socialsData.js';
+
+const socialsHTML = renderSocials(socialsData);
+
+console.log(socialsHTML);
