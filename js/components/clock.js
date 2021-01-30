792118a7ac76c6fdd69a8740c2848ecffdb4d414
Rimantas <belauzas@yahoo.com>
2021-01-30 03:17:14 +0200

    Tick tock

--- js/components/clock.js
+++ js/components/clock.js
@@ -18,7 +18,7 @@ function clock(selector, deadline) {
         return false;
     }
 
-    const numbers = {
+    let numbers = {
         days: 432,
         hours: 9,
         minutes: 37,
@@ -40,25 +40,62 @@ function clock(selector, deadline) {
     // result
     DOM.innerHTML = HTML;
 
+    // neefektyvi paieska, nes ieskoma visame dokumente (HTML'e)
+    // const allValuesDOM = document.querySelectorAll(`${selector} .value`);
+    // zymiai efektyvesne paieska, nes pradeda nuo jau zinomos artimiausios tinkamos vietos
+    const allValuesDOM = DOM.querySelectorAll('.value');
+
     setInterval(function () {
-        updateClock(DOM, deadline);
+        numbers = updateClock(deadline);
+        for (let i = 0; i < 4; i++) {
+            const key = labels[i];
+            allValuesDOM[i].innerText = numbers[key];
+        }
     }, 1000);
 }
 
-function updateClock(DOM, deadline) {
+function updateClock(deadline) {
     const date = new Date();
 
     let deadlineYear = date.getFullYear();
     let fullDeadline = `${deadlineYear}-${deadline}`;
-    const deadlineInMiliseconds = (new Date(fullDeadline)).getTime();
+    let deadlineInMiliseconds = (new Date(fullDeadline)).getTime();
     const now = Date.now();
 
     if (deadlineInMiliseconds < now) {
         deadlineYear++;
         fullDeadline = `${deadlineYear}-${deadline}`;
+        deadlineInMiliseconds = (new Date(fullDeadline)).getTime();
     }
 
-    console.log(fullDeadline);
+    const diff = deadlineInMiliseconds - now;
+
+    // let unusedSeconds = Math.round(diff / 1000);
+
+    // const days = Math.floor(unusedSeconds / 60 / 60 / 24);
+    // unusedSeconds -= days * 60 * 60 * 24;
+
+    // const hours = Math.floor(unusedSeconds / 60 / 60);
+    // unusedSeconds -= hours * 60 * 60;
+
+    // const minutes = Math.floor(unusedSeconds / 60);
+
+    // const seconds = unusedSeconds - minutes * 60;
+
+    let unusedTime = Math.round(diff / 1000);
+
+    const seconds = unusedTime % 60;
+    unusedTime = (unusedTime - seconds) / 60;
+
+    const minutes = unusedTime % 60;
+    unusedTime = (unusedTime - minutes) / 60;
+
+    const hours = unusedTime % 24;
+    unusedTime = (unusedTime - hours) / 24;
+
+    const days = unusedTime;
+
+    return { days, hours, minutes, seconds };
 }
 
 export { clock }
